2f93033be2997954ca680ac83bd29dbb692b4960
shahabrostami <shbrostami@gmail.com>
2014-12-03 14:50:36 +0000

    Revert "Flag representation, changed UI, fixed some bugs."
    This reverts commit d0e5f9e2fbd0647ac66e0fcaa0f628826a0c440f.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 /* 
 		Created by Shahab Rostami
-		rostami@uk.ibm.com
+		shbrostami@gmail.com
 		
 		Module dependencies
 		- Express
@@ -18,12 +18,9 @@ var express = require('express')
 	, io = require('socket.io').listen(http)
 	, _ = require('underscore');
 
-var host = 'localhost';
-var port = 3000;
-
 /* Set up Server Configuration */
-app.set('ip', host);
-app.set('port', port);
+app.set('ip', '127.0.0.1');
+app.set('port', '8080');
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 
@@ -34,9 +31,11 @@ app.use(express.bodyParser());
 /* Set up participant array 
 Struct: id (sessionID), name (nickname), language, isAnon (true/false)*/
 var users = [];
+var services = JSON.parse(process.env.VCAP_SERVICES || "{}");
+
 /* Setup HTTP options, including translation URI and langauges array 
-   SET UP WTS INSTANCE URL HERE */
-var translateURL = "";
+   SET UP TRANSLATION SERVICE URL HERE */
+var translateURL = null;
 var languages = [];
 var options = {
       uri: translateURL,
@@ -61,7 +60,8 @@ var callbackSupportedLanguages = function(error, res, body)
     }
 };
 // Send a request to return all languages supported, and when complete, store these into an array.
-request(options, callbackSupportedLanguages);
+if(translateURL != null)
+	request(options, callbackSupportedLanguages);
 
 /* Handle the translation of text */
 var translateText = function(data, functionCallback)
@@ -113,10 +113,10 @@ function getAnonId(users) {
 
 /* Simple heartbeat to keep clients from timing out */
 function sendHeartbeat(){
-    setTimeout(sendHeartbeat, 10000);
+    setTimeout(sendHeartbeat, 8000);
     io.sockets.emit('ping', { beat : 1 });
 }
-setTimeout(sendHeartbeat, 10000);
+setTimeout(sendHeartbeat, 8000);
 
 /* All socket.IO events are handled here, this is the real-time engine we're using */
 io.on('connection', function(socket){
@@ -124,18 +124,21 @@ io.on('connection', function(socket){
 	var newAnonId = getAnonId(users);
 	socket.emit('initChat', {newAnonId : newAnonId, languages : languages});
 	
-    /* Once the user registers,  update the user list and notify other users connected. */
+	/* Function to handle heartbeat on client connection */
+	socket.on('pong', function(data){
+        console.log("Pong received from client");
+    });
+	
+	/* Once the user registers,  update the user list and notify other users connected. */
 	socket.on('registerUser', function(data){
-        
-            users.push({
-                id : data.id, 
-                name : data.name,
-                language: data.language,
-                isAnon : data.isAnon
-            });
-            users = _.uniq(users);
-            // Notify other users.
-            io.emit('userConnected', {users: users, name: data.name, language: data.language});
+		users.push({
+			id : data.id, 
+			name : data.name,
+			language: data.language,
+			isAnon : data.isAnon
+		});
+		// Notify other users.
+		io.emit('userConnected', {users: users, name: data.name});
 	});
 	
 	/* What to do if we receive a message */
@@ -152,7 +155,7 @@ io.on('connection', function(socket){
 			// If the user returned is the same as the user that sent the message, simply send the non-translated message back.
 			if(toUser.id === fromUser.id)
 			{
-				toSocket.emit('newMessage', {name: fromUser.name, message: data.message, fromLanguage: fromUser.language});
+				toSocket.emit('newMessage', {name: fromUser.name, message: data.message});
 			}
 			// Else, we know that the user may potentially require a translation of the message to be sent.
 			else
@@ -160,22 +163,28 @@ io.on('connection', function(socket){
 				// If the user receiving the message has the same language as the user that sent the message, simply send the non-translated message back.
 				if(toUser.language == fromUser.language)
 				{
-					toSocket.emit('newMessage', {name: fromUser.name, message: data.message, fromLanguage: fromUser.language});
+					toSocket.emit('newMessage', {name: fromUser.name, message: data.message});
 				}
 				else
 				{
 					// Else, we know that a translation is required, translate the message and send to the appropriate user when completed.
-					translateText({toUser : toUser, fromUser : fromUser, text : data.message, fromLanguage: fromUser.language},
+					translateText({toUser : toUser, fromUser : fromUser, text : data.message},
 					function (error, response, body) {
 						if(!error && response.statusCode == 200)
 						{
 							toSocket = io.sockets.connected[response.request.headers['toUser']];
-							toSocket.emit('newMessage', {name: fromUser.name, message: body, fromLanguage: fromUser.language});
+							toSocket.emit('newMessage', {name: fromUser.name, message: body});
 						}
 					});
 				}
 			}
 		}
+	});
+	
+	/* What to do if language has been changed */
+	socket.on('updateLanguage', function(data) {
+		// Find that user and update their language.
+		_.findWhere(users, {id:socket.id}).language = data.language;
 	});
 	
 	// If user disconnects, update variables as required.
@@ -184,28 +193,13 @@ io.on('connection', function(socket){
 		user = _.findWhere(users, {id: socket.id});
 		
 		// Remove the user from the list.
-		if(typeof(user) != "undefined")
-		{
-			users = _.without(users, user);
-			io.emit('userDisconnected', {users: users, name: user.name, language: user.language});
-		}
+		users = _.without(users, user);
+		io.emit('userDisconnected', {users: users, name: user.name});
   });
-    
-    /* Function to handle heartbeat on client connection */
-	socket.on('pong', function(data){
-        console.log("Heartbeat from client");
-    });
 });
 
 /* HTTP server start at defined port and ip address. */
 http.listen(app.get('port'), app.get('ip'), function() {
 	console.log('Up and running, go to ' + app.get('ip') + ':' + app.get('port'));
 });
-
-process.on( 'SIGINT', function() {
-  console.log( "\nGracefully shutting down from SIGINT (Ctrl-C)" );
-  shutDownMessage = "Server is currently unavailable.";
-  io.emit('shutdown', {message: shutDownMessage});
-  process.exit( );
-})
 	
--- public/js/index.js
+++ public/js/index.js
@@ -1,133 +1,87 @@
 function init() {
 
-    // Initialise when webpage is ready.
-    $('#message').focus();
+	// Initialise when webpage is ready.
+	$('#message').focus();
 
-    // Set base server url and connect to socket.
-    var url = document.domain;
-    var socket = io.connect(url, {'reconnect': false});
+	// Set base server url and connect to socket.
+	var url = document.domain;
+	var socket = io.connect(url);
 
-    // We'll save our session ID in a variable for later
-    var sessionId = '';
+	// We'll save our session ID in a variable for later
+	var sessionId = '';
     var noOfUsers = 0;
-    var language = 'en';
-    
-    var languageDict = {};
-    languageDict["en"] = "English (US)";
-    languageDict["es"] = "Spanish";
-    languageDict["fr"] = "French";
-    
-    function scrollToBottom(){
-         $("#chatWindow").scrollTop($("#chatWindow")[0].scrollHeight)
-    }
-    function updateUsers(users, name, language, message) {
-        // Return user who has joined or left.
-        $("#chatWindow").append('<hr><p><img src="/img/' + language + '.png"/><b><i>' + name + ' has ' + message + '</i></b></p><hr>');
-        scrollToBottom();
-        $("#userWindow").html("");
-        for(var i = 0; i < users.length; i++)
-            $("#userWindow").append('<p><img src="/img/' + users[i].language + '.png"/</t><b>' + users[i].name + '</b></p>');
-    }
-    
-    // Simple client heartbeat to keep from timing out.
-    socket.on('ping', function(data){
-        socket.emit('pong', {beat: 1});
+	
+	function updateUsers(users, name, message) {
+		// Return user who has joined or left.
+		$("#chatWindow").append("" + name + " has " + message + ".\n");
+		$("#userWindow").html("");
+		for(var i = 0; i < users.length; i++)
+			$("#userWindow").append("" + users[i].name + "\n");
+	}
+	
+	// Simple client heartbeat to keep from timing out.
+	socket.on('ping', function(data){
+      socket.emit('pong', {beat: 1});
     });
-    
-    // On client successful connect, record sessionID of client.
-    socket.on('connect', function () {
-        // Set the client's sessionId
-        sessionId = socket.io.engine.id;
-       
-        socket.on('initChat', function(data) {
-            // Prompt user for name
-            
-                var languages = data.languages;
-                $.each(languages, function(key, value){
-                     $('#languages')
-                        .append($("<option></option>")
-                        .attr("value",value.name)
-                        .text(languageDict[value.name])); 
-                });
-                
-                var startDialog = $( "#dialog" ).dialog({
-                    close: function( event, ui ) {},
-                    resizable: false,
-                    modal: false,
-                    draggable: true,
-                    width: 400,
-                    open: function(event, ui){setTimeout("$('#dialog').dialog('close')",20000); },
-                    position: { my: "center", at: "center", of: window },
-                    buttons: {
-                    "OK": function() {
-                        $( this ).dialog( "close" );
-                        },
-                    Cancel: function() {
-                        $( this ).dialog( "close" );
-                        }
-                    }
-                });
-                
-                var form = startDialog.find( "form" ).on( "submit", function( event ) {
-                  event.preventDefault();
-                  startDialog.dialog( "close" );
-                });
-                
-            $( "#dialog" ).on( "dialogclose", function( event, ui ) {
-                // var name = prompt("What name will you use?\nLeave empty or cancel to be anonymous.");
-                // Check to see if user is anonymous
-                name = $("#nameStartup").val();
-                if(name === null || name === "")
-                    name = "Anonymous" + data.newAnonId;
-                $("#name").val(name);
-                language = $("#languages").val();
-                $("#language").append('<img src="/img/' + language + '.png"/>');
-                // $("#name").val(name);
-                // Set languages available. Need to make this less derp.
-                // Update all connected users.
-                
-                socket.emit('registerUser', {id: sessionId, name: name, language: $("#languages").val(), isAnon: true});
-            });
-        });
-    });
-    
-    socket.on('newMessage', function(data) {
-            if(data.fromLanguage != language)
-            {
-                $("#chatWindow").append('<p><img src="/img/' + data.fromLanguage + '.png"/><b>' + data.name + ' [translated  from ' + languageDict[data.fromLanguage] + ']</b>: ' + data.message + '</br></p>');
-            }
-            else
-                $("#chatWindow").append('<p><img src="/img/' + data.fromLanguage + '.png"/><b>' + data.name + '</b>: ' + data.message + '</br></p>');
-            scrollToBottom();
-        });
-        
-        // Notify and update when a user connects.
-        socket.on('userConnected', function(data) {
-            updateUsers(data.users, data.name, data.language, "joined");
-        });
-        
-        // Notify and update when a user leaves.
-        socket.on('userDisconnected', function(data) {
-            updateUsers(data.users, data.name, data.language, "left");
-        });
-        
-        socket.on('shutdown', function(data) {
-            window.location.replace("/offline.html");
-        });
-    
-    // Once user has submitted a messge, notify server.
-    function sendMessage(e) {
-        // If enter key is pressed, user has submitted a message.
-        if(e.which == 13)
-        {
-            var message = $('#message').val();
-            $('#message').val('');
-            socket.emit('sendMessage', {message: message, language: language});
-            }
-    }
-    
-    // Element actions referenced here.
-    $('#message').on('keydown', sendMessage);
+	
+	// On client successful connect, record sessionID of client.
+	socket.on('connect', function () {
+		// Set the client's sessionId
+		sessionId = socket.io.engine.id;
+		
+		socket.on('initChat', function(data) {
+			// Prompt user for name
+			var name = prompt("What name will you use?\nLeave empty or cancel to be anonymous.");
+			// Check to see if user is anonymous
+			if(name === null || name === "")
+				name = "Anonymous" + data.newAnonId;
+			$("#name").val(name);
+			
+			// Set languages available. Need to make this less derp.
+			var languages = data.languages;
+			$.each(languages, function(key, value){
+				 $('#languages')
+					.append($("<option></option>")
+					.attr("value",value.name)
+					.text(value.name)); 
+			});
+			// Update all connected users.
+			socket.emit('registerUser', {id: sessionId, name: name, language:"en", isAnon: true});
+		});
+		
+		socket.on('newMessage', function(data) {
+			$("#chatWindow").append("" + data.name + ": " + data.message + "\n");
+		});
+		
+		// Notify and update when a user connects.
+		socket.on('userConnected', function(data) {
+			updateUsers(data.users, data.name, "joined");
+		});
+		
+		// Notify and update when a user leaves.
+		socket.on('userDisconnected', function(data) {
+			updateUsers(data.users, data.name, "left");
+		});
+	});
+	
+	// Once user has submitted a messge, notify server.
+	function sendMessage(e) {
+		// If enter key is pressed, user has submitted a message.
+		if(e.which == 13)
+		{
+			var message = $('#message').val();
+			$('#message').val('');
+			var language = $('#languages').val();
+			socket.emit('sendMessage', {message: message, language: language});
+			}
+	}
+	
+	// Element actions referenced here.
+	$('#message').on('keydown', sendMessage);
+	$( "#languages" ).change(function() {
+		var language = $('#languages').val();
+		socket.emit('updateLanguage', {language: language});
+	});
 };
 
 $(document).on('ready', init);
